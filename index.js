791d71356e842ebcb9fa66c138a7d7bdf1bd87c1
ShuveeDev <157414258+ShuveeDev@users.noreply.github.com>
2025-03-13 14:32:06 +0200

    Коммит 2

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const express = require('express')
-
+const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.set('view engine')
 
+app.set('view engine', 'ejs');
+
+// Налаштовуємо публічну папку для CSS
+app.use(express.static('public'));
+
+// Головна сторінка
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Головна' });
+});
+
+// Сторінка "Про нас"
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'Про нас' });
+});
+
+
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Контакти' });
+});
+
+app.listen(port, () => {
+    console.log(`Сайт працює на http://localhost:${port}`);
+});
 console.log('Server Started at [PORT}')
